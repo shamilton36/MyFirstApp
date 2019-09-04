e2989655d750182263832c4b8e23497fb407bc03
parkerbryant <pbryant8@gatech.edu>
2019-09-04 00:57:55 -0400

    Attempted to add firebase functionality

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,20 +1,64 @@
 package com.example.myfirstapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    // Create a new user with a first and last name
+    public void saveUser(View view) {
+
+        Map<String, Object> user = new HashMap<>();
+        EditText userView = (EditText) (findViewById(R.id.editText));
+        EditText passView = (EditText) (findViewById(R.id.editPassword));
+        String userText = userView.getText().toString();
+        String passText = passView.getText().toString();
+        user.put("Username", userText);
+        user.put("Password", passText);
+
+        // Add a new document with a generated ID
+        db.collection("users").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+    }
+
+
+
 
     /**called when the user taps the send button */
     public void sendMessage(View view) {
