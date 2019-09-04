2adebff5c9c9c0a76c6e2487d1f650ac12529a39
shamilton36 <52138178+shamilton36@users.noreply.github.com>
2019-09-03 22:28:05 -0400

    Changed the layout to have username and password

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -7,9 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 public class MainActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference mDatabaseReference = mDatabase.getReference();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
+        mDatabaseReference = mDatabase.getReference().child("name");
+        mDatabaseReference.setValue(message);
+
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
